b773d3056c16260a769d1ef9437f8bc858cd74b3
Matt <45896519+Skater92@users.noreply.github.com>
2022-07-02 13:25:36 -0400

    UPDATE 13.25 7/2/2022

--- DnD_Encounter_Manager/Monster.cs
+++ DnD_Encounter_Manager/Monster.cs
@@ -28,7 +28,7 @@ namespace DnD_Encounter_Manager
         public string? ConImmun { get; set; }
         public string? Sense { get; set; }
         public string? Language { get; set; }
-        public string? Feature { get; set; }
+        public List<string>? Feature { get; set; }
         public string? Action { get; set; }
         public int LegendaryAct { get; set; }
 
---
+++ DnD_Encounter_Manager/MonsterBuilders/MonsterManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnD_Encounter_Manager.MonsterBuilders
+{
+    public class MonsterManager
+    {
+        private Monster newMon = new Monster();
+        public List<Monster> monsters = new List<Monster>();
+
+        public void addFeatures(List<string> inputList)
+        {
+            newMon.Feature = inputList;           
+        }
+
+        public void addAction()
+        {
+
+        }
+
+        public void buildMon(int id, int AC, int HP, string Name, bool isLegendary, bool inLair, string size, int speed, int str, int dex,
+            int con, int Int, int Wis, int Cha)
+        {
+            newMon.MonsterID = id;
+            newMon.AC = AC;
+            newMon.HP = HP;
+            newMon.Name = Name;
+            newMon.size = size;
+            newMon.speed = speed;
+            newMon.Str = str;
+            newMon.Dex = dex;
+            newMon.Con = con;
+            newMon.Int = Int;
+            newMon.Wis = Wis;
+            newMon.Cha = Cha;
+
+            if(isLegendary)
+            {
+                newMon.isLegendary = true;
+                newMon.LegendaryAct = 3;
+                if(inLair)
+                {
+                    newMon.inLair = true;
+                }
+            }
+
+        }
+
+        public void addMon()
+        {
+            monsters.Add(newMon);
+        }
+
+    }
+}
